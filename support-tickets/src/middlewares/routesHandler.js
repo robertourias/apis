35c4d84c982d07b349d0ef5e7eda794954ed5390
Roberto Nicoletti <roberto.urias@gmail.com>
2024-11-14 14:38:57 -0300

    routesHandler para lidar com a busca da rota e metodo

---
+++ support-tickets/src/middlewares/routesHandler.js
@@ -0,0 +1,14 @@
+import { routes } from "../routes";
+
+export function routesHandler(request, response) {
+  const route = routes.find((route) => {
+    return route.method === request.method && route.path === request.url;
+  });
+
+  if(route) {
+    return route.controller(request, response);
+  } 
+    
+  return response.writeHead(404, { "Content-Type": "text/plain" }).end("Not Found");  
+}
+  
