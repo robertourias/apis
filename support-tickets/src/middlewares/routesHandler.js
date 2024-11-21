da051da00070295ba468f8e1f872227432c6d862
Roberto Nicoletti <roberto.urias@gmail.com>
2024-11-21 15:23:58 -0300

    Criando o banco de dados

--- support-tickets/src/middlewares/routesHandler.js
+++ support-tickets/src/middlewares/routesHandler.js
@@ -1,4 +1,7 @@
 import { routes } from "../routes/index.js";
+import { DataBase } from "../database/database.js";
+
+const database = new DataBase();
 
 export function routesHandler(request, response) {
   const route = routes.find((route) => {
@@ -6,7 +9,7 @@ export function routesHandler(request, response) {
   });
 
   if(route) {
-    return route.controller({request, response});
+    return route.controller({request, response, database});
   } 
     
   return response.writeHead(404, { "Content-Type": "text/plain" }).end("Not Found");  
