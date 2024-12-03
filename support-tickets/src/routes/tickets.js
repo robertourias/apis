b2a48051cc8c9eb87955a35d2784da5bc5a17ab2
Roberto Nicoletti <roberto.urias@gmail.com>
2024-12-03 18:13:14 -0300

    Rota para atualizar ticket

--- support-tickets/src/routes/tickets.js
+++ support-tickets/src/routes/tickets.js
@@ -1,15 +1,21 @@
-import { create } from "../controllers/tickets/create.js";
-import { index } from "../controllers/tickets/index.js";
+import { create } from "../controllers/tickets/create.js"
+import { index } from "../controllers/tickets/index.js"
+import { update } from "../controllers/tickets/update.js"
 
 export const tickets = [
   {
-    method: 'POST',
-    path: '/tickets',
-    controller: create
+    method: "POST",
+    path: "/tickets",
+    controller: create,
   },
   {
-    method: 'GET',
-    path: '/tickets',
-    controller: index
-  }
+    method: "GET",
+    path: "/tickets",
+    controller: index,
+  },
+  {
+    method: "PUT",
+    path: "/tickets/:id",
+    controller: update,
+  },
 ]
