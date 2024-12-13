610367da4da52ad543530289e1b2b45d1e128445
Roberto Nicoletti <roberto.urias@gmail.com>
2024-12-13 17:02:20 -0300

    Removendo um ticket

--- support-tickets/src/database/database.js
+++ support-tickets/src/database/database.js
@@ -54,4 +54,13 @@ export class DataBase {
       this.#persist()
     }
   }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id)
+
+    if (rowIndex !== -1) {
+      this.#database[table].splice(rowIndex, 1)
+      this.#persist()
+    }
+  }
 }
